1b6db34df2f261ae57d2651f7988019151334c46
James Olsen <olsenjames1116@gmail.com>
2023-01-09 22:01:06 -0700

    Create DOM elements and players factory functions

--- javascript.js
+++ javascript.js
@@ -1,7 +1,32 @@
-const startButton = (function () {
-    const buttonElement = document.querySelector('button');
-    buttonElement.addEventListener('click', () => {
-        const form = document.querySelector('form');
-        form.setAttribute('style', 'display: block');
-    });
-})();
+const elementFactory = (selector) => {
+    const element = document.querySelector(selector);
+    return { element };
+};
+
+const startButton = elementFactory('button#startButton');
+const form = elementFactory('form');
+
+startButton.element.addEventListener('click', () => {
+    form.element.setAttribute('style', 'display: block');
+});
+
+const playerFactory = (name) => {
+    const displayName = () => {
+        console.log(`Player: ${name}`);
+    };
+    return { name, displayName };
+};
+
+const playButton = elementFactory('button#playButton');
+
+playButton.element.addEventListener('click', () => {
+    const playerOneInput = elementFactory('input#playerOneName');
+    const playerOneName = playerOneInput.element.value;
+    const playerTwoInput = elementFactory('input#playerTwoName');
+    const playerTwoName = playerTwoInput.element.value;
+
+    const playerOne = playerFactory(playerOneName);
+    const playerTwo = playerFactory(playerTwoName);
+    playerOne.displayName();
+    playerTwo.displayName();
+});
