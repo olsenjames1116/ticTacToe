1c575e2c6f5761229c77d23c10c473d413ab2cb2
James Olsen <olsenjames1116@gmail.com>
2023-01-10 09:43:00 -0700

    Create and display gameboard
    The gameboard is created in the play button event listener so it will
    appear when the user is ready to start the game.

--- javascript.js
+++ javascript.js
@@ -1,32 +1,49 @@
 const elementFactory = (selector) => {
-    const element = document.querySelector(selector);
-    return { element };
+    const findElement = document.querySelector(selector);
+    return { findElement };
 };
 
 const startButton = elementFactory('button#startButton');
 const form = elementFactory('form');
 
-startButton.element.addEventListener('click', () => {
-    form.element.setAttribute('style', 'display: block');
+startButton.findElement.addEventListener('click', () => {
+    form.findElement.setAttribute('style', 'display: block');
 });
 
-const playerFactory = (name) => {
+const playerFactory = (name, symbol) => {
     const displayName = () => {
         console.log(`Player: ${name}`);
     };
-    return { name, displayName };
+    return { name, symbol, displayName };
+};
+
+const gameboardFactory = function () {
+    const makeBoard = () => {
+        const gameboardElement = document.createElement('div');
+        gameboardElement.classList.add('gameboard');
+        document.querySelector('body').appendChild(gameboardElement);
+
+        for (let i = 0; i < 9; i += 1) {
+            const gameSquareElement = document.createElement('div');
+            gameboardElement.appendChild(gameSquareElement);
+        }
+    };
+    return { makeBoard };
 };
 
 const playButton = elementFactory('button#playButton');
 
-playButton.element.addEventListener('click', () => {
+playButton.findElement.addEventListener('click', () => {
     const playerOneInput = elementFactory('input#playerOneName');
-    const playerOneName = playerOneInput.element.value;
+    const playerOneName = playerOneInput.findElement.value;
     const playerTwoInput = elementFactory('input#playerTwoName');
-    const playerTwoName = playerTwoInput.element.value;
+    const playerTwoName = playerTwoInput.findElement.value;
 
-    const playerOne = playerFactory(playerOneName);
-    const playerTwo = playerFactory(playerTwoName);
+    const playerOne = playerFactory(playerOneName, 'X');
+    const playerTwo = playerFactory(playerTwoName, 'O');
     playerOne.displayName();
     playerTwo.displayName();
+
+    const gameboard = gameboardFactory();
+    gameboard.makeBoard();
 });
